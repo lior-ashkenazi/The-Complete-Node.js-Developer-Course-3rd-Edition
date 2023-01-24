daa9315b7bbcff0ec9481201fbfb94694ba939ec
Lior Ashkenazi <lior.ashkenazi>
2023-01-25 00:08:27 +0200

    Section 11 - before lecture 87 - Structuring a REST API

---
+++ task-manager/src/db/mongoose.js
@@ -0,0 +1,77 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api");
+
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail()) throw new Error("Email is invalid");
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 7,
+    trim: true,
+    validate(value) {
+      if (value.toLowerCase().includes("password"))
+        throw new Error("Password cannot contain 'password'");
+    },
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) throw new Error("Age must be a positive number");
+    },
+  },
+});
+
+// const me = new User({
+//   name: "Lior",
+//   age: 27,
+// });
+//
+// me.save()
+//   .then((me) => {
+//     console.log(me);
+//   })
+//   .catch((error) => {
+//     console.log("Error!", error);
+//   });
+
+const Task = mongoose.mode("Task", {
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+// const task = new Task({
+//   description: "Learn the Mongoose library",
+//   completed: false,
+// });
+//
+// task
+//   .save()
+//   .then((task) => {
+//     console.log(task);
+//   })
+//   .catch((error) => {
+//     console.log("Error!", error);
+//   });
