65291512faf09ca79350f9be85ea7d8a9f2fde74
Lior Ashkenazi <lior.ashkenazi>
2023-01-26 14:37:36 +0200

    Completed Section 11

--- task-manager/playground/promise-chaining-2.js
+++ task-manager/playground/promise-chaining-2.js
@@ -1,13 +1,27 @@
 require("../src/db/mongoose");
 const Task = require("../src/models/task");
 
-Task.findByIdAndDelete("63d02fd4fec1e6dffc4aa12d")
-  .then((task) => {
-    console.log(task);
-    return Task.countDocuments({ completed: false });
-  })
-  .then((result) => {
-    console.log(result);
+// Task.findByIdAndDelete("63d02fd4fec1e6dffc4aa12d")
+//   .then((task) => {
+//     console.log(task);
+//     return Task.countDocuments({ completed: false });
+//   })
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((e) => {
+//     console.log(e);
+//   });
+
+const deleteTaskAndCount = async (id) => {
+  await Task.findByIdAndDelete(id);
+  const count = await Task.countDocuments({ completed: false });
+  return count;
+};
+
+deleteTaskAndCount("63d02fd4fec1e6dffc4aa12d")
+  .then((count) => {
+    console.log(count);
   })
   .catch((e) => {
     console.log(e);
