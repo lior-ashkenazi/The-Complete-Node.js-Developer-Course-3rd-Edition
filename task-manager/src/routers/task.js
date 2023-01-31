ac419b996894a3d4825e466fdbc56dba5f6774a2
Lior Ashkenazi <lior.ashkenazi>
2023-01-31 14:00:22 +0200

    Completed Section 13

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -14,11 +14,33 @@ router.post("/tasks", auth, async (req, res) => {
   }
 });
 
+// GET /tasks?completed=true
+// GET /tasks?limit=10&skip=20
+// GET /tasks?sortBy=createdAt:desc
 router.get("/tasks", auth, async (req, res) => {
+  const match = {};
+  const sort = {};
+
+  if (req.query.completed) match.completed = req.query.completed === "true";
+
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split(":");
+    sort[parts[0]] = parts[1] === "desc" ? -1 : 1;
+  }
+
   try {
-    const tasks = await Task.find({ owner: req.user._id });
-    // await req.user.populate("tasks");
-    res.send(tasks);
+    await req.user.populate({
+      path: "tasks",
+      match,
+      options: {
+        // if not a number or undefined,
+        // Mongoose will ignore it
+        limit: parseInt(req.query.limit),
+        skip: parseInt(req.query.skip),
+        sort,
+      },
+    });
+    res.send(req.user.tasks);
   } catch (e) {
     res.status(500).send();
   }
