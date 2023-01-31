8dd89479ca6ddd17f4452d6f9bbcec80651b5eb5
Lior Ashkenazi <lior.ashkenazi>
2023-01-31 12:59:56 +0200

    Completed Section 12

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -8,26 +8,34 @@ const taskRouter = require("./routers/task");
 const app = express();
 const port = process.env.PORT || 3000;
 
+// app.use((req, res, next) => {
+//   if (req.method === "GET") {
+//     res.send("GET requests are disabled");
+//   } else {
+//     next();
+//   }
+// });
+
+// app.use((req, res, next) => {
+//   res.status(503).send("Site is currently down. Check back soon!");
+// });
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
-const bcrypt = require("bcryptjs")
-
-const myFunction = async () => {
-  const password = "Red12345!"
-
-  // 8 resembles the number of round we want to
-  // the algorithm to iterate on the plaintext
-  // 8 "strikes" balance between security and speed
-  // recommended by the algorithm's creator
-  const hashedPassword = await bcrypt.hash(password, 8)
-
-  const isMatch = await bcrypt.compare("Red12345!", hashedPassword)
-}
-
-myFunction()
-
 app.listen(port, () => {
   console.log("Server is up on port", port);
 });
+
+// const main = async () => {
+//   // const task = await Task.findById("63d7f48b138bf9023ec2f9e5");
+//   // await task.populate("owner");
+//   // console.log(task.owner);
+//
+//   const user = await User.findById("63d7f475138bf9023ec2f9df");
+//   await user.populate("tasks");
+//   console.log(user.tasks);
+// };
+//
+// main();
--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const Task = require("./task");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -35,7 +37,42 @@ const userSchema = new mongoose.Schema({
       if (value < 0) throw new Error("Age must be a positive number");
     },
   },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
 });
+
+userSchema.virtual("tasks", {
+  ref: "Task",
+  localField: "_id",
+  foreignField: "owner",
+});
+
+userSchema.methods.toJSON = function () {
+  const user = this;
+  const userObject = user.toObject();
+
+  delete userObject.password;
+  delete userObject.tokens;
+
+  return userObject;
+};
+
+userSchema.methods.generateAuthToken = async function () {
+  const user = this;
+  // jwt is expecting a String ID rather than Object ID
+  const token = jwt.sign({ _id: user._id.toString() }, "thisismynewcourse");
+
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+
+  return token;
+};
 
 userSchema.statics.findByCredentials = async (email, password) => {
   const user = await User.findOne({ email });
@@ -60,6 +97,17 @@ userSchema.pre("save", async function (next) {
   if (user.isModified("password")) {
     user.password = await bcrypt.hash(user.password, 8);
   }
+
+  // next is called in the end of those function for
+  // signaling that the function done with it's async operations
+  next();
+});
+
+// Delete user tasks when user is removed
+userSchema.pre("remove", async function (next) {
+  const user = this;
+
+  await Task.deleteMany({ owner: user._id });
 
   next();
 });
