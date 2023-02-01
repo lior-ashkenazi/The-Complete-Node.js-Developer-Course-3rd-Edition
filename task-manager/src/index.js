fb882ee6aac0284c6297047782479226d0b984cd
Lior Ashkenazi <lior.ashkenazi>
2023-02-01 18:43:03 +0200

    Completed Section 14. Section 15 won't be implemented (no support for it anymore

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,24 +1,10 @@
 const express = require("express");
 require("./db/mongoose");
-const User = require("./models/user");
-const Task = require("./models/task");
 const userRouter = require("./routers/user");
 const taskRouter = require("./routers/task");
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-// app.use((req, res, next) => {
-//   if (req.method === "GET") {
-//     res.send("GET requests are disabled");
-//   } else {
-//     next();
-//   }
-// });
-
-// app.use((req, res, next) => {
-//   res.status(503).send("Site is currently down. Check back soon!");
-// });
 
 app.use(express.json());
 app.use(userRouter);
@@ -27,15 +13,3 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log("Server is up on port", port);
 });
-
-// const main = async () => {
-//   // const task = await Task.findById("63d7f48b138bf9023ec2f9e5");
-//   // await task.populate("owner");
-//   // console.log(task.owner);
-//
-//   const user = await User.findById("63d7f475138bf9023ec2f9df");
-//   await user.populate("tasks");
-//   console.log(user.tasks);
-// };
-//
-// main();
